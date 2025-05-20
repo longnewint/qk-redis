d20ee209e6f6076ac9013bd85b0b64bb541eba5f
Long Nguyen <longnewint@gmail.com>
2025-05-20 15:20:12 -0400

    Change quarkus version to 3.20.1

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,28 @@
 package org.acme;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 @Path("/city")
 public class GreetingResource {
+  @Inject RedissonClient client;
 
   @GET
   @Produces(MediaType.TEXT_PLAIN)
   public String hello() {
-    return "Hello from Quarkus REST";
+
+    var map = client.getMap("city:653", StringCodec.INSTANCE);
+    System.out.println(map.readAllEntrySet());
+
+    return "nani";
   }
 }
